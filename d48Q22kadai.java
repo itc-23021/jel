986d02dd32a339e142a8ea1e04aacd7c0252b97f
itc-23021 <s23021@std.it-college.ac.jp>
2024-08-09 01:04:32 +0000

    課題提出忘れ

---
+++ d48Q22kadai.java
@@ -0,0 +1,12 @@
+// s23021
+// d48Q22のコードをコンパイルエラーしないように修正
+enum Connection {OFFLINE, ONLINE}
+class d48Q22kadai {
+		public static void main(String[] args) {
+				Connection s = Connection.OFFLINE;
+				if (s == Connection.OFFLINE) {
+				   s = Connection.ONLINE;
+				}
+				System.out.print(s);
+		}
+}
